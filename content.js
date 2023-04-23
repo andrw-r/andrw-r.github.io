4f3b5d6b6690f1d53fc73b45171961744b6eefe6
Andrew <32582710+tr1L@users.noreply.github.com>
2023-04-22 23:24:06 -0230

    Added bio and contact folders.

--- content.js
+++ content.js
@@ -1,56 +1,28 @@
-// Load the content of the bio page
 function loadBioContent() {
-    const xhr = new XMLHttpRequest();
-    xhr.onreadystatechange = function() {
-      if (this.readyState == 4 && this.status == 200) {
-        document.getElementById("bio-content").innerHTML = this.responseText;
-      }
-    };
-    xhr.open("GET", "bio-content.html", true);
-    xhr.send();
-    // Get the bio-content div
-    var bioContentDiv = document.getElementById("bio-content");
-
-    // Create a new XMLHttpRequest object
-    var xhttp = new XMLHttpRequest();
-
-    // Set up the callback function for when the request is complete
-    xhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-        // Replace the contents of the bio-content div with the response text
-        bioContentDiv.innerHTML = this.responseText;
-    }
-    const bioContent = document.getElementById("bio-content");
-
-    if (bioContent) {
-        fetch("bio-content.html")
-        .then((response) => response.text())
-        .then((data) => {
-        bioContent.innerHTML = data;
-    });
-}
-
-};
-
-// Open the request
-xhttp.open("GET", "bio-content.html", true);
-
-// Send the request
-xhttp.send();
-
-  }
+    fetch('/bio/bio-content.html')
+      .then(response => response.text())
+      .then(data => {
+        document.getElementById('bio-content').innerHTML = data;
+      })
+      .catch(error => {
+        console.error('Error loading bio index.html:', error);
+      });
   
-// Load the content of the contact page
+    }
+
+  
 function loadContactContent() {
-    const xhr = new XMLHttpRequest();
-    xhr.onreadystatechange = function() {
-      if (this.readyState == 4 && this.status == 200) {
-        document.getElementById("contact-content").innerHTML = this.responseText;
-      }
-    };
-    xhr.open("GET", "contact-content.html", true);
-    xhr.send();
-  }
+    fetch('/contact/contact-content.html')
+      .then(response => response.text())
+      .then(data => {
+        document.getElementById('contact-content').innerHTML = data;
+      })
+      .catch(error => {
+        console.error('Error loading contact index.html:', error);
+      });
+  
+    }
+
   
   // Load the content of the index page
   function loadIndexContent() {
@@ -67,7 +39,7 @@ function loadContactContent() {
   // Navigate to the bio page
   function goToBio() {
     loadBioContent();
-    window.location.href = "bio.html";
+    window.location.href = "/bio";
   }
   
   // Navigate to the contact page
@@ -79,6 +51,6 @@ function loadContactContent() {
   // Navigate to the index page
   function goToIndex() {
     loadIndexContent();
-    window.location.href = "index.html";
+    window.location.href = "../";
   }
   
